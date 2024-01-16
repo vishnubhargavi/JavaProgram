5c4b6e4208c96fdd5e06c5903003adac1a19b80a
vishnubhargavi <vishnubhargavi6912@gmail.com>
2024-01-16 02:36:48 -0500

    FirstJavaProgram

---
+++ src/Student.java
@@ -0,0 +1,12 @@
+public class Student {
+        String name= "vishnu";
+        public String getName() {
+
+            return name;
+        }
+        public static void main(String[] args){
+
+            Student studentName = new Student();
+            System.out.println(studentName.getName());
+        }
+    }
